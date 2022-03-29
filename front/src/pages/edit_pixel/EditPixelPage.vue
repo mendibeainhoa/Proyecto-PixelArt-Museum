f8d1fbc1c099c92fb13d7acff37d674d023481d0
ainhoa <mendibeainhoa@gmail.com>
2022-03-29 15:01:20 +0200

    principio de boton de borrar, un poco de refactorizacion de codigo y css

--- front/src/pages/edit_pixel/EditPixelPage.vue
+++ front/src/pages/edit_pixel/EditPixelPage.vue
@@ -1,104 +1,192 @@
 
 <template>
-  <div id="Contenedor">
-    <h1><p>Grid Canvas</p></h1>
-    <div id="contenedorCanvas">
-      <canvas id="canvas1" width="400" height="300"> </canvas>
-      <section>
-        <div
-          class="canva"
-          v-bind:style="{ backgroundColor: colors[0] }"
-          @click="onPixelClicked(0)"
-        ></div>
-        <div
-          class="canva"
-          v-bind:style="{ backgroundColor: colors[1] }"
-          @click="onPixelClicked(1)"
-        ></div>
-        <div
-          class="canva"
-          v-bind:style="{ backgroundColor: colors[2] }"
-          @click="onPixelClicked(2)"
-        ></div>
-        <div
-          class="canva"
-          v-bind:style="{ backgroundColor: colors[3] }"
-          @click="onPixelClicked(3)"
-        ></div>
-        <div
-          class="canva"
-          v-bind:style="{ backgroundColor: colors[4] }"
-          @click="onPixelClicked(4)"
-        ></div>
-        <div
-          class="canva"
-          v-bind:style="{ backgroundColor: colors[5] }"
-          @click="onPixelClicked(5)"
-        ></div>
-        <div
-          class="canva"
-          v-bind:style="{ backgroundColor: colors[6] }"
-          @click="onPixelClicked(6)"
-        ></div>
-        <div
-          class="canva"
-          v-bind:style="{ backgroundColor: colors[7] }"
-          @click="onPixelClicked(7)"
-        ></div>
-        <div
-          class="canva"
-          v-bind:style="{ backgroundColor: colors[8] }"
-          @click="onPixelClicked(8)"
-        ></div>
-        <div
-          class="canva"
-          v-bind:style="{ backgroundColor: colors[9] }"
-          @click="onPixelClicked(9)"
-        ></div>
-      </section>
-    </div>
-    <!-- <input type="range" id="sizeCuadros" value="20" min="5" max="100" /> -->
-    <section id="colorPicker">
-      <input
-        type="button"
-        class="canva"
-        id="color-0"
-        v-bind:style="{ backgroundColor: selectedColor[0] }"
-        @click="onColorChange(0)"
-      />
-      <input
-        type="button"
-        class="canva"
-        id="color-1"
-        v-bind:style="{ backgroundColor: selectedColor[1] }"
-        @click="onColorChange(1)"
-      />
-      <input
-        type="button"
-        class="canva"
-        id="color-2"
-        v-bind:style="{ backgroundColor: selectedColor[2] }"
-        @click="onColorChange(2)"
-      />
-      <input
-        type="button"
-        class="canva"
-        id="color-3"
-        v-bind:style="{ backgroundColor: selectedColor[3] }"
-        @click="onColorChange(3)"
-      />
-      <input
-        type="color"
-        id="color-4"
-        v-model="selectedColor[4]"
-        @click="onPickerButton(4)"
-      />
-    </section>
-    <p>{{ selectedColor }}</p>
-    <!-- <section>
-      <button class="checker" @click="onPickerButton(4)">colorear</button>
-    </section> -->
-  </div>
+  <h1><p>Grid Canvas</p></h1>
+  <section class="pixel-canva">
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[0] }"
+      @click="onPixelClicked(0)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[1] }"
+      @click="onPixelClicked(1)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[2] }"
+      @click="onPixelClicked(2)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[3] }"
+      @click="onPixelClicked(3)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[4] }"
+      @click="onPixelClicked(4)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[5] }"
+      @click="onPixelClicked(5)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[6] }"
+      @click="onPixelClicked(6)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[7] }"
+      @click="onPixelClicked(7)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[8] }"
+      @click="onPixelClicked(8)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[9] }"
+      @click="onPixelClicked(9)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[10] }"
+      @click="onPixelClicked(10)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[11] }"
+      @click="onPixelClicked(11)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[12] }"
+      @click="onPixelClicked(12)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[13] }"
+      @click="onPixelClicked(13)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[14] }"
+      @click="onPixelClicked(14)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[15] }"
+      @click="onPixelClicked(15)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[16] }"
+      @click="onPixelClicked(16)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[17] }"
+      @click="onPixelClicked(17)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[18] }"
+      @click="onPixelClicked(18)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[19] }"
+      @click="onPixelClicked(19)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[20] }"
+      @click="onPixelClicked(20)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[21] }"
+      @click="onPixelClicked(21)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[22] }"
+      @click="onPixelClicked(22)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[23] }"
+      @click="onPixelClicked(23)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[24] }"
+      @click="onPixelClicked(24)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[25] }"
+      @click="onPixelClicked(25)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[26] }"
+      @click="onPixelClicked(26)"
+    ></div>
+    <div
+      class="canva"
+      v-bind:style="{ backgroundColor: colors[27] }"
+      @click="onPixelClicked(27)"
+    ></div>
+  </section>
+
+  <section id="colorPicker">
+    <input
+      type="button"
+      class="canva"
+      id="color-0"
+      v-bind:style="{ backgroundColor: selectedColor[0] }"
+      @click="onColorChange(0)"
+    />
+    <input
+      type="button"
+      class="canva"
+      id="color-1"
+      v-bind:style="{ backgroundColor: selectedColor[1] }"
+      @click="onColorChange(1)"
+    />
+    <input
+      type="button"
+      class="canva"
+      id="color-2"
+      v-bind:style="{ backgroundColor: selectedColor[2] }"
+      @click="onColorChange(2)"
+    />
+    <input
+      type="button"
+      class="canva"
+      id="color-3"
+      v-bind:style="{ backgroundColor: selectedColor[3] }"
+      @click="onColorChange(3)"
+    />
+    <input
+      type="color"
+      id="color-4"
+      v-model="selectedColor[4]"
+      @click="onPickerButton(4)"
+    />
+  </section>
+  <input
+    type="button"
+    id="borrador"
+    @click="onResetColor()"
+    value="Borrar color"
+  />
+  <p>{{ selectedColor }}</p>
 </template>
 
 <script>
@@ -106,8 +194,37 @@ export default {
   data() {
     return {
       selectedColor: ["red", "blue", "green", "yellow"],
-      colors: ["", "", "", "", "", "", "", "", ""],
+      colors: [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+      ],
       index: 0,
+      borrador: "",
     };
   },
   methods: {
@@ -122,36 +239,47 @@ export default {
     onPixelClicked(position) {
       this.index = position;
     },
+    onResetColor() {
+      this.colors = this.borrador;
+    },
   },
 };
 </script>
 
 <style scoped >
-section {
-  display: flex;
-  justify-content: center;
+* {
+  margin: 0;
+  padding: 0;
 }
-div {
-  color: rgb(0, 0, 0);
-  border: 1px solid black;
+h1 {
   margin: 1em;
 }
+.pixel-canva {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-left: 35%;
+  width: 30%;
+  border: solid 1px black;
+}
+div {
+  margin: 0.1em;
+  justify-content: center;
+}
+
 #colorPicker {
   display: flex;
   justify-content: center;
-}
-.checker {
   margin: 1em;
 }
-.position {
+input {
   margin: 1em;
 }
 .canva {
-  width: 20px;
-  height: 20px;
-  margin: 1em;
+  width: 30px;
+  height: 30px;
+  border: 1px solid black;
 }
 </style>
 
 // https://codepen.io/JesusTepec/pen/pjzpOE
-// https://www.w3schools.com/html/html5_canvas.asp
